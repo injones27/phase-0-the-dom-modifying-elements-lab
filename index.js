2794a4dce833a74ff01ea13b8e2b53068e02e4f3
Isaiah Jones <injones27@gmail.com>
2023-05-07 22:10:01 -0400

    Creating and Inserting DOM Nodes Lab Completed!

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+// newHeader has id of "victory"
+// document.body.(newHeader);
+// newHeader.setAttribute("id", "victory");
+// newHeader.add("id", "victory");
+newHeader.id = "victory";
+newHeader.textContent = "Isaiah is the champion"
